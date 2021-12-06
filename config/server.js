e0e1dd303966441019bd87849342a9eb93b99d93
Roberto-mondaca <Roberto-mondaca>
2021-12-06 15:30:59 -0300

    asd

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ca21193810c13556c8da1c54c7a041d8'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9a72bf73-2fc1-4cf8-aa81-f278eaac879a'
+};
